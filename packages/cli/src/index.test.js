fddf25bd315842f955f80efd0f9a6f703f561134
Urip Subekti <urip.subekti13@gmail.com>
2022-11-09 12:18:06 +0700

    chore: add test file

---
+++ packages/cli/src/index.test.js
@@ -0,0 +1,23 @@
+const { exec } = require("child_process");
+const { existsSync } = require("fs");
+const path = require("path");
+
+describe("Generate i18n json Test", () => {
+  const execAsync = async (cmd) => {
+    return new Promise((resolve, reject) => {
+      exec(cmd, (err, stdout, stderr) => {
+        if (err) return reject(stderr);
+        return resolve(stdout);
+      });
+    });
+  };
+
+  test("should generate json", async () => {
+    const id = `12kxsNITg_kHRCs7DUYUOqzj-tXzdO3r0nGUYMHvAQlo`;
+    const indexFilePath = path.join(__dirname, "index.js");
+    const outputFilename = "test-data.json";
+    const outputFilePath = path.join(__dirname, "..", outputFilename);
+    await execAsync(`node ${indexFilePath} pull --spreadsheet-id ${id} --out ${outputFilename}`);
+    expect(existsSync(outputFilePath)).toEqual(true);
+  });
+});
